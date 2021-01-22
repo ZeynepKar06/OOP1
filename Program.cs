400452fae1e0625ba3f1674929cccc9b56bafd97
ZeynepKar06 <77546727+ZeynepKar06@users.noreply.github.com>
2021-01-22 22:19:23 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,13 +1,25 @@
 ﻿using System;
 
-namespace GenericsIntro
+namespace OOP1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            MyList<string> isimler = MyList<string>();
-            isimler.Add("Zeynep");
+            Product product1 = new Product() { };
+            product1.Id = 1;
+            product1.CategoryId = 2;
+            product1.ProductName = "Masa";
+            product1.UnitPrice = 500;
+            product1.UnitStock = 3;
+
+            Product product2 = new Product {Id=2,CategoryId=5,UnitStock=5,ProductName="Kalem",UnitPrice=35 };
+
+            ProductManager productManager = new ProductManager ();
+            productManager.Add(product1);
+            Console.WriteLine(product1.ProductName);
+
+            
 
         }
     }
